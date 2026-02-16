6e5887df780e1bbc9ed86bcfe0523cc9f4e82752
umukundwaa <a.umukundwa1@alustudent.com>
2026-02-16 07:27:57 -0800

    add Generetic class

---
+++ Generetic/Pair.java
@@ -0,0 +1,36 @@
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value){
+        if (key == null) {
+            throw new IllegalArgumentException("Key cannot be null !!");
+        }
+
+        this.key = key;
+        this.value = value;
+
+
+    }
+
+     public static <K, V> Pair<K, V> create(K key, V value) {
+        return new Pair<>(key, value);
+    }
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+
+@Override
+public String toString() {
+    return "(" +
+            "Key:" + key +
+            ", Value:" + value  +
+            ')';
+}
+}
+
