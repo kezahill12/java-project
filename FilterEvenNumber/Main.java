a24f3ab0bacf0a53dcd84de686d170f051ee7e5e
Umukundwaa <a.umukundwa1@alustudent.com>
2026-02-20 19:59:26 +0200

    Change input list to include even numbers only

--- FilterEvenNumber/Main.java
+++ FilterEvenNumber/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> numbers=List.of(1,3,5,9,7,11);
+        List<Integer> numbers=List.of(0,2,4,6,8,10);
         List<Integer> evennumbers=EvenNumberFilter.filterEvenNumbers(numbers);
         System.out.println("The orginal numbers are: " + numbers);
         System.out.println("The even numbers are: " + evennumbers);
