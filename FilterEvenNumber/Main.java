1c12a25a70116124ef5e423e66448bfedc192a71
Umukundwaa <a.umukundwa1@alustudent.com>
2026-02-20 20:00:13 +0200

    Change numbers list to be empty

--- FilterEvenNumber/Main.java
+++ FilterEvenNumber/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> numbers=List.of(0,2,4,6,8,10);
+        List<Integer> numbers=List.of( );
         List<Integer> evennumbers=EvenNumberFilter.filterEvenNumbers(numbers);
         System.out.println("The orginal numbers are: " + numbers);
         System.out.println("The even numbers are: " + evennumbers);
